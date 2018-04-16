9eac78348ed4c3604cfe6bcc89974fd531de7bb7
sikanderc <sikanderchowhan@gmail.com>
2018-04-16 09:37:11 -0400

    added 2 methods to reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,18 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let array = str.split("")
-  array.reverse()
-  return array.join("")
+  // let array = str.split("")
+  // array.reverse()
+  // return array.join("")
+
+  //without reverse method
+  // let reversed = ""
+  // for (let character of str) {
+  //   reversed = character + reversed
+  // }
+  // return reversed
+
+  return str.split("").reduce((rev, char) => char + rev, "")
 }
 
 module.exports = reverse;
