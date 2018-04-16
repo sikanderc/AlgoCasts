2e4c5d0ae12ea9e616f76d57e504ba5dad27f082
sikanderc <sikanderchowhan@gmail.com>
2018-04-16 10:59:16 -0400

    cleaned up reverseInt function

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,13 +9,7 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  if (Math.sign(n)===-1) {
-    let num = n.toString().split("").splice(1)
-    let newInt = parseInt(num.reverse().join(""))
-    return -newInt
-  } else {
-    return parseInt(n.toString().split("").reverse().join(""))
-  }
+  return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n)
 }
 
 module.exports = reverseInt;
