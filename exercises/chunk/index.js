83ccd83d4b9a95d80e866d727f77b66b522b7573
sikanderc <sikanderchowhan@gmail.com>
2018-04-16 18:52:48 -0400

    chunk is chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,12 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunkedArray = []
+  for (let i = 0; i < array.length;) {
+    chunkedArray.push(array.splice(i, size))
+  }
+  return chunkedArray
+}
 
 module.exports = chunk;
