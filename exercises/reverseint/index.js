7cdd43af421d4d06606745270198a477b6fab9bf
sikanderc <sikanderchowhan@gmail.com>
2018-04-16 10:52:35 -0400

    reversed int first method

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if (Math.sign(n)===-1) {
+    let num = n.toString().split("").splice(1)
+    let newInt = parseInt(num.reverse().join(""))
+    return -newInt
+  } else {
+    return parseInt(n.toString().split("").reverse().join(""))
+  }
+}
 
 module.exports = reverseInt;
